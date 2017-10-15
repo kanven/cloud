7d3fe8f206ec867030f7ee3f5e90d0cb932080cd
kanven <yuanlong_jiang@sina.com>
2017-10-15 11:26:26 +0800

    controller调整

--- cloud-client/src/main/java/com/kanven/cloud/client/controller/ClientController.java
+++ cloud-client/src/main/java/com/kanven/cloud/client/controller/ClientController.java
@@ -1,17 +1,21 @@
 package com.kanven.cloud.client.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping(value = "/client")
 public class ClientController {
 
-	@RequestMapping(value = "/hello/{name}", method = RequestMethod.GET)
-	public String hello(@PathVariable String name) {
-		return "hello," + name;
+	@Autowired
+	private RestTemplate template;
+
+	@RequestMapping(value = "/test/{name}")
+	public String brave(@PathVariable String name) {
+		return template.getForObject("http://localhost:8080/cloud/brave/hello/{name}", String.class, name);
 	}
 
 }
--- cloud-server/src/main/java/com/kanven/cloud/server/controller/BraveController.java
+++ cloud-server/src/main/java/com/kanven/cloud/server/controller/BraveController.java
@@ -1,21 +1,17 @@
 package com.kanven.cloud.server.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping(value = "/brave")
 public class BraveController {
 
-	@Autowired
-	private RestTemplate template;
-
-	@RequestMapping(value = "/test/{name}")
-	public String brave(@PathVariable String name) {
-		return template.getForObject("http://localhost:8080/cloud/client/hello/{name}", String.class, name);
+	@RequestMapping(value = "/hello/{name}", method = RequestMethod.GET)
+	public String hello(@PathVariable String name) {
+		return "hello," + name;
 	}
 
 }
