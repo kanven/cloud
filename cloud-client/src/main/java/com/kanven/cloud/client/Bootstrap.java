4ae5b6d28aceb6eb02d1423ef3ac6eac3ea23d58
kanven <yuanlong_jiang@sina.com>
2017-10-15 09:32:38 +0800

    brave调试

--- cloud-client/src/main/java/com/kanven/cloud/client/Bootstrap.java
+++ cloud-client/src/main/java/com/kanven/cloud/client/Bootstrap.java
@@ -2,12 +2,16 @@ package com.kanven.cloud.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
+@PropertySource(value = { "classpath:brave.properties" })
 @SpringBootApplication
 public class Bootstrap {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Bootstrap.class, args);
 	}
-	
+
 }
--- cloud-server/src/main/java/com/kanven/cloud/server/Bootstrap.java
+++ cloud-server/src/main/java/com/kanven/cloud/server/Bootstrap.java
@@ -2,7 +2,11 @@ package com.kanven.cloud.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
+@PropertySource(value={"classpath:brave.properties"})
 @SpringBootApplication
 public class Bootstrap {
 
